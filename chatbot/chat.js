8b736070e0b56c4382afaca09cb488f71dddd9fc
ricardomelogt <ricardofrontend@gmail.com>
2022-08-26 11:54:16 -0300

    menu reciprev

--- chatbot/chat.js
+++ chatbot/chat.js
@@ -183,28 +183,127 @@ const robotReact = (mensagemUser) => {
           break;
 
         // PREVIDENCIA
-        case resposta.indexOf('previdência') !== -1:
-        case resposta.indexOf('previdencia') !== -1:
-          console.log('direcionar para Previdência');
+        case resposta.indexOf('reciprev') !== -1:
+        case resposta.indexOf('reciprev') !== -1:
           robotDialog(`<p>No <strong>Plano de Previdência</strong> posso lhe ajudar em:</p>
             <div class="chat-btn" onclick="enviarMsg('Calendário de pagamento')">Calendário de pagamento</div>
-            <div class="chat-btn" onclick="enviarMsg('Situação de processo')">Situação de processo</div>`);
+            <div class="chat-btn" onclick="enviarMsg('Acompanhamento de processos')">Acompanhamento de processos</div>
+            <div class="chat-btn" onclick="enviarMsg('Aposentadoria')">Aposentadoria</div>
+            <div class="chat-btn" onclick="enviarMsg('Auxílio funeral')">Auxílio funeral</div>
+            <div class="chat-btn" onclick="enviarMsg('CADPREV/DIPR')">CADPREV/DIPR</div>
+            <div class="chat-btn" onclick="enviarMsg('Concessão de pensão')">Concessão de pensão</div>
+            <div class="chat-btn" onclick="enviarMsg('Contribuições previdenciárias')">Contribuições previdenciárias</div>
+            <div class="chat-btn" onclick="enviarMsg('Certificado de Regularidade Previdenciária')">Certificado de Regularidade Previdenciária</div>
+            <div class="chat-btn" onclick="enviarMsg('Estudo de Aderência')">Estudo de Aderência</div>
+            <div class="chat-btn" onclick="enviarMsg('Extrato previdenciário')">Extrato previdenciário</div>
+            <div class="chat-btn" onclick="enviarMsg('Gestão atuarial')">Gestão atuarial</div>
+            <div class="chat-btn" onclick="enviarMsg('Prova de anual de vida')">Prova anual de vida</div>
+            <div class="chat-btn" onclick="enviarMsg('Plano de Trabalho Atuarial')">Plano de Trabalho Atuarial</div>
+            <div class="chat-btn" onclick="enviarMsg('Passivo judicial')">Passivo judicial</div>
+            <div class="chat-btn" onclick="enviarMsg('Previdência complementar')">Previdência complementar</div>
+            <div class="chat-btn" onclick="enviarMsg('Restos deixados')">Restos deixados</div>
+            <div class="chat-btn" onclick="enviarMsg('Reciprev serviços')">Reciprev serviços</div>
+            `);
           break;
 
         // PREVIDENCIA > CALENDÁRIO DE PAGAMENTO
         case resposta.indexOf('calendário') !== -1:
         case resposta.indexOf('calendario') !== -1:
-        case resposta.indexOf('pagamento') !== -1:
-          console.log('direcionar para Calendário de pagamento');
           robotDialog(`<p>Direcionar para Calendário de pagamento</p>`);
           break;
         
-        // PREVIDENCIA > SITUAÇÃO DE PROCESSO
+        // PREVIDENCIA > ACOMPANHAMENTO DE PROCESSO
         case resposta.indexOf('processo') !== -1:
-        case resposta.indexOf('situação de processo') !== -1:
-          console.log('direcionar para Situação de processo');
-          robotDialog(`<p>Para verificar o status do processo, por favor informe o número do processo aqui: </p><span class='line-space'></span><input class='msg-input' type="number" placeholder='Digite aqui o número do processo...'/>
-          <div class="chat-btn" onclick="robotDialog('Estou verificando o processo...')">Verificar processo</div>`);
+        case resposta.indexOf('processos') !== -1:
+        case resposta.indexOf('acompanhamento de processos') !== -1:
+          window.location.href = "https://recifeemdia.recife.pe.gov.br/acompanhamentoProcesso/";
+          break;
+        
+        // PREVIDENCIA > APOSENTADORIA
+        case resposta.indexOf('aposentadoria') !== -1:
+          window.location.href = "https://reciprev.recife.pe.gov.br/aposentadoria";
+          break;
+
+        // PREVIDENCIA > AUXILIO FUNERAL
+        case resposta.indexOf('funeral') !== -1:
+        case resposta.indexOf('auxilio') !== -1:
+        case resposta.indexOf('auxílio') !== -1:
+          window.location.href = "https://reciprev.recife.pe.gov.br/auxilio-funeral";
+          break;
+
+        // PREVIDENCIA > CADPREV/DIPR
+        case resposta.indexOf('cadprev') !== -1:
+        case resposta.indexOf('dipr') !== -1:
+        case resposta.indexOf('cadprev/dipr') !== -1:
+          window.location.href = "https://reciprev.recife.pe.gov.br/cadprevdipr";
+          break;
+        
+        // PREVIDENCIA > CONCESSÃO DE PENSÃO
+        case resposta.indexOf('pensão') !== -1:
+          window.location.href = "https://reciprev.recife.pe.gov.br/concessao-de-pensao";
+          break;
+
+        // PREVIDENCIA > CONTRIBUIÇÕES PREVIDENCIÁRIAS
+        case resposta.indexOf('contribuições') !== -1:
+        case resposta.indexOf('previdenciarias') !== -1:
+        case resposta.indexOf('previdenciárias') !== -1:
+          window.location.href = "https://reciprev.recife.pe.gov.br/contribuicoes-previdenciarias";
+          break;
+        
+        // PREVIDENCIA > CERTIFICADO DE REGULARIDADE PREVIDENCIÁRIA - CRP 
+        case resposta.indexOf('certificado') !== -1:
+        case resposta.indexOf('regularidade') !== -1:
+        case resposta.indexOf('crp') !== -1:
+          window.location.href = "https://reciprev.recife.pe.gov.br/certificado-de-regularidade-previdenciaria-crp";
+          break;
+
+        // PREVIDENCIA > ESTUDO DE ADERÊNCIA
+        case resposta.indexOf('estudo') !== -1:
+        case resposta.indexOf('aderencia') !== -1:
+        case resposta.indexOf('aderência') !== -1:
+          window.location.href = "https://reciprev.recife.pe.gov.br/sites/default/files/inline-files/Relat%C3%B3rio_Pro_Gest%C3%A3o_RECIFE%28PE%29_2019_Nivel_I_II_III.pdf";
+          break;
+
+        // PREVIDENCIA > EXTRATO PREVIDENCIÁRIO
+        case resposta.indexOf('extrato') !== -1:
+          window.location.href = "https://login.recife.pe.gov.br/auth/realms/recife/protocol/openid-connect/auth?response_type=code&redirect_uri=https%3A%2F%2Fportaldoservidor.recife.pe.gov.br%2Flogin_check&client_id=pose&nonce=5890f8c4be350d734ea452d5c792dc29&state=602b7cdc6e952a4d09872d2a45d74dbb&scope=openid";
+          break;
+
+        // PREVIDENCIA > GESTÃO ATUARIAL
+        case resposta.indexOf('atuarial') !== -1:
+          window.location.href = "https://reciprev.recife.pe.gov.br/gestao-atuarial";
+          break;
+
+        // PREVIDENCIA > PROVA ANUAL DE VIDA
+        case resposta.indexOf('prova') !== -1:
+          window.location.href = "https://reciprev.recife.pe.gov.br/prova-de-vida";
+          break;
+
+        // PREVIDENCIA > PLANO DE TRABALHO ATUARIAL
+        case resposta.indexOf('trabalho') !== -1:
+          window.location.href = "https://reciprev.recife.pe.gov.br/sites/default/files/inline-files/relatorio_normas_procedimentos_atuariais_v10%20%282%29_2.pdf";
+          break;
+
+        // PREVIDENCIA > PASSIVO JUDICIAL
+        case resposta.indexOf('passivo') !== -1:
+        case resposta.indexOf('judicial') !== -1:
+          window.location.href = "https://reciprev.recife.pe.gov.br/sites/default/files/inline-files/RELAT%C3%93RIO%20DE%20AVALIA%C3%87%C3%83O%20DO%20PASSIVO%20JUDICIAL%20-%202021.2.pdf";
+          break;
+
+        // PREVIDENCIA > PREVIDÊNCIA COMPLEMENTAR
+        case resposta.indexOf('complementar') !== -1:
+          window.location.href = "https://reciprev.recife.pe.gov.br/previdencia-complementar";
+          break;
+
+        // PREVIDENCIA > RESTOS DEIXADOS
+        case resposta.indexOf('restos') !== -1:
+        case resposta.indexOf('deixados') !== -1:
+          window.location.href = "https://reciprev.recife.pe.gov.br/restos-deixados";
+          break;
+
+        // PREVIDENCIA > RECIPREV SERVIÇOS
+        case resposta.indexOf('serviços') !== -1:
+          window.location.href = "https://reciprev.recife.pe.gov.br/reciprev-servicos";
           break;
 
         default:
